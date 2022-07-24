0620a41a26ad7c77a01a6d1d9f74e6c659f8e739
Troy <troy@ZipcodersMBP28.fios-router.home>
2022-07-24 10:38:32 -0400

    checkpoint - add, remove, size, get

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -4,4 +4,110 @@ package com.zipcodewilmington.singlylinkedlist;
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedList {
+    private Node head;
+    private static int counter;
+
+    //Inner Node Class
+    private class Node {
+        Object data;
+        Node next;
+
+        //Constructor
+        public Node(Object dataValue) {
+            next = null;
+            data = dataValue;
+        }
+        public Node(Object dataValue, Node nextValue) {
+            next = nextValue;
+            data = dataValue;
+        }
+        public Object getData(){
+            return data;
+        }
+        public void setData(Object dataValue){
+            data = dataValue;
+        }
+        //get next data aka the tail of node
+        public Node getNext(){
+            return next;
+        }
+
+        public void setNext(Node nextValue){
+            next = nextValue;
+        }
+    }
+
+    private static int getCounter(){
+        return counter;
+    }
+    private static void incrementCounter(){
+        counter++;
+    }
+    private void decrementCounter(){
+        counter--;
+    }
+
+
+    public boolean add(Object data){
+
+        boolean valueAdded = false;
+
+        if(head == null){
+            head = new Node(data);
+            valueAdded = true;
+        }
+        Node temp = new Node(data);
+        Node current = head;
+
+        if(current != null){
+            while (current.getNext() != null) {
+                current = current.getNext();
+            }
+            current.setNext(temp);
+            valueAdded = true;
+        }
+        incrementCounter();
+        return valueAdded;
+    }
+
+    public int size(){
+        return getCounter();
+    }
+
+    public Object get(int index){
+        if (index < 0) {
+            return null;
+        }
+        Node current = null;
+        if(head != null){
+            current = head.getNext();
+            for (int i = 0; i < index; i++){
+                if(current.getNext() == null)
+                    return null;
+                    current = current.getNext();
+
+            }
+            return current.getData();
+        }
+        return current;
+    }
+
+    public boolean remove(int index){
+        if(index < 1 || index > size()){
+            return false;
+        }
+        Node current = head;
+        if(head != null){
+            for(int i = 0; i < index; i++){
+                if(current.getNext() == null)
+                    return false;
+                current = current.getNext();
+            }
+            current.setNext(current.getNext().getNext());
+            decrementCounter();
+            return true;
+        }
+        return false;
+    }
+
 }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,58 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
+
+    @Test
+    public void testAdd(){
+        SinglyLinkedList node = new SinglyLinkedList();
+        node.add(5);
+        node.add(9);
+
+        Assert.assertTrue(node.add(9));
+    }
+
+    @Test
+    public void testSize(){
+        SinglyLinkedList node = new SinglyLinkedList();
+        node.add(5);
+        node.add(9);
+        int expected = 2;
+
+        Assert.assertEquals(expected, node.size());
+    }
+
+    @Test
+    public void testGet(){
+        SinglyLinkedList node = new SinglyLinkedList();
+        node.add(5);
+        node.add(9);
+        int expected = 9;
+
+        Assert.assertEquals(expected, node.get(1));
+    }
+
+    @Test
+    public void testRemove(){
+        SinglyLinkedList node = new SinglyLinkedList();
+        node.add(5);
+        node.add(9);
+        node.remove(1);
+        int expected = node.size();
+        int actual = 1;
+
+        Assert.assertEquals(expected, actual);
+    }
+
+
+
+
+
+
+
 }
